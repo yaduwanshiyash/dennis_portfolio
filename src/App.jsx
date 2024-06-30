a355958eb6c07fc26a07c182e3ed79f3ca79e4a2
yash yadav <132824732+yaduwanshiyash@users.noreply.github.com>
2024-06-30 11:10:33 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomePage from './components/LandingPage/HomePage'
+import Page2 from './components/Page2/Page2'
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+      <Page2 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Page2/Page2Animation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from './Image'
+import TextDiv from './TextDiv'
+
+const Page2Animation = () => {
+  return (
+    <div className='mt-10'>
+        
+        <TextDiv />
+    </div>
+  )
+}
+
+export default Page2Animation
